746e0f3a228cd83d22a0df45c01cda9ab46afa15
GiaHeplBro <nguyenduonggia63@gmail.com>
2025-07-12 22:36:26 +0700

    Add history and change UI bottom

---
+++ app/src/main/java/com/example/mini_project_prm/fragments/PurchaseHistoryFragment.kt
@@ -0,0 +1,17 @@
+package com.example.mini_project_prm.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.mini_project_prm.R
+
+class PurchaseHistoryFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_purchase_history, container, false)
+    }
+}
