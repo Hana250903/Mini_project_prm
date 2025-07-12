f278f4499e6859772b89f1b9837da873ceef58d7
Hana250903 <kiritomiku0@gmail.com>
2025-07-12 23:44:42 +0700

    fix: zalopay

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -81,7 +81,7 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
 
     implementation(fileTree(mapOf(
-        "dir" to "D:\\figure\\app\\libs",
+        "dir" to "E:\\AndroidProject\\Mini_project_prm\\app\\libs",
         "include" to listOf("*.aar", "*.jar"),
         "exclude" to listOf("")
     )))
